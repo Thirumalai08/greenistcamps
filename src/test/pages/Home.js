74689dd786455165b3058a2b1acbe1a032aadf7b
Thirumalai08 <thirunambi422@gmail.com>
2020-11-13 11:10:34 +0530

    Tours Added

--- src/test/pages/Home.js
+++ src/test/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from '../components/Banner'
 import Features from '../components/Features'
+import Services from '../components/Services'
 
 function Home(){
     return(
         <div>
             <Banner />
             <Features />
+            <Services />
         </div>
     )
 }
